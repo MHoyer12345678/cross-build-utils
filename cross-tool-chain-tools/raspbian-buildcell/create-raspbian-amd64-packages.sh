2302fa1de8f4e6e9028651df629729770b1ef178
MHoyer12345678 <mhoyer.oss-devel@freenet.de>
2022-11-13 09:26:02 +0100

    Created script to convert whole package list to raspbian distro.

--- cross-tool-chain-tools/raspbian-buildcell/create-raspbian-amd64-packages.sh
+++ cross-tool-chain-tools/raspbian-buildcell/create-raspbian-amd64-packages.sh
@@ -6,3 +6,48 @@
 # - remove :amd64
 # - remove essential libs and rasp gcc from list
 
+if [ $# -eq 0 ]; then
+    echo "Missing file with list of packages."
+    exit 1
+fi
+
+pkg_list_success=""
+pkg_list_version_conflict=""
+pkg_list_no_amd64=""
+
+pkg_list_fn=$1
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+while IFS= read -r pkg
+do
+    echo "Converting pkg: $pkg"
+    $SCRIPT_DIR/convert-pkg-deb-rasp.sh ${pkg}
+    case $? in
+	0)
+	    pkg_list_sucess="${pkg_list_success}${pkg} "
+	    ;;
+
+	2)
+	    pkg_list_no_amd64="${pkg_list_no_amd64}${pkg} "
+	    ;;
+
+	3)
+	    pkg_list_version_conflict="${pkg_list_version_conflict}${pkg} "
+	    ;;
+
+	*)
+	    echo "Unrecoverable error. Exiting ..."
+	    exit 1
+	    ;;
+esac
+
+done < "$pkg_list_fn"
+
+echo "---------------------------- Done -------------------------------------"
+echo "List of successfully ported packages: ${pkg_list_success}"
+echo ""
+echo "Packages not AMD64: ${pkg_list_no_amd64}"
+echo ""
+echo "Packages with version conflicts: ${pkg_list_version_conflict}"
+echo ""
